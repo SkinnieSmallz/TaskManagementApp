3befcfca2a7581f13cff51a21395e80fc45ebab6
belmo <belmo@6F2EDA7BCFFB557>
2025-06-13 01:59:52 +0200

    Add dashboard page

---
+++ TaskManagement.Web/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagement.Web.Controllers
+{
+    [Authorize] // ensures only logged in users can access
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
